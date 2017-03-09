75c16c96cc3873a96b5274f31ced0d68b30b344a
James Dinsdale <hi@molovo.co>
2017-03-09 12:37:54 +0000

    Only deploy index file

--- build.zsh
+++ build.zsh
@@ -1,15 +1,16 @@
 #!/usr/bin/env zsh
 
-echo '[' >! index.json
+[[ -d build ]] || mkdir -p build
+echo '[' >! build/index.json
 local -a _package_list
 _package_list=(./packages/*)
 if [[ ${#_package_list[@]} -gt 0 ]]; then
-	cat "${_package_list[1]}" >> index.json
+	cat "${_package_list[1]}" >> build/index.json
   _package_list[1]=()
 	for f in "${_package_list[@]}"; do
-		echo "," >> index.json
-		cat "$f" >> index.json
+		echo "," >> build/index.json
+		cat "$f" >> build/index.json
 	done
 fi
-echo ']' >> index.json
+echo ']' >> build/index.json
 
