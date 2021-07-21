1a42d9e4a1c47ecdbb21d551e2081e5741b27c2c
Henpai Hsu <dreamfliper@gmail.com>
2021-07-21 21:47:22 +0800

    Add z.lua package

---
+++ packages/z.lua
@@ -0,0 +1,8 @@
+{
+  "name": "z.lua",
+  "description": "A command line tool which helps you navigate faster by learning your habits ",
+  "repository": "https://github.com/skywind3000/z.lua",
+  "author": "skywind3000",
+  "type": "script",
+  "init": {"z.lua.plugin.zsh": "z"}
+}
